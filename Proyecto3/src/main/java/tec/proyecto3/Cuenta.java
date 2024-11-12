56bd495911401209af578bd011c9d2a98708020b
Allajox <allajox22@gmail.com>
2024-11-12 17:10:04 -0600

    Commit inicial

---
+++ Proyecto3/src/main/java/tec/proyecto3/Cuenta.java
@@ -0,0 +1,21 @@
+package tec.proyecto3;
+
+/**
+ *
+ * @author allaj
+ */
+public class Cuenta {
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contraseña;
+
+    public Cuenta(String nombre, String apellido, String correo, String contraseña) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+    
+    
+}
---
+++ Proyecto3/src/main/java/tec/proyecto3/SistemaIniciarSesión.java
@@ -0,0 +1,20 @@
+package tec.proyecto3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author allaj
+ */
+public class SistemaIniciarSesión {
+    private Cuenta cuenta;
+    private List<Cuenta> cuentas;
+
+    public SistemaIniciarSesión(Cuenta cuenta, List<Cuenta> cuentas) {
+        this.cuenta = cuenta;
+        this.cuentas = cuentas;
+    }
+    
+    
+}

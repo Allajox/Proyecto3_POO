6827d410ebda91bbbc76d179e5213920ad2d37b6
Allajox <allajox22@gmail.com>
2024-11-13 01:46:22 -0600

    Cambios en documentación

--- Proyecto3/src/main/java/tec/proyecto3/ValidarCredenciales.java
+++ Proyecto3/src/main/java/tec/proyecto3/ValidarCredenciales.java
@@ -6,18 +6,23 @@ package tec.proyecto3;
  * @author allaj
  */
 public class ValidarCredenciales {
-    
+    /**
+     * Valida si la contraseña tiene al menos una letra mayúscula, un número y un carácter especial
+     * 
+     * @param contraseña String a ser validado
+     * @return true si la contraseña cumple los requisitos
+     */
     public static boolean validarContraseña(String contraseña) {
-        // Expresión regular que valida si la contraseña tiene al menos:
-        // - Una letra mayúscula: (?=.*[A-Z])
-        // - Un número: (?=.*\\d)
-        // - Un carácter especial: (?=.*[@#$%^&+=])
         String regexContraseña = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+-=¡!¿?'<>:;]).{8,}$";
-        
         return contraseña.matches(regexContraseña);
         }
-    public static boolean validarCorreo(String correo) {
-        
+    /**
+     * Valida si el formato del correo es correcto, o sea, si tiene un @ en medio
+     * 
+     * @param correo String a ser validado
+     * @return true si el correo cumple los requisitos
+     */
+    public static boolean validarCorreo(String correo) { 
         String regexCorreo = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
         return correo.matches(regexCorreo);
     }
